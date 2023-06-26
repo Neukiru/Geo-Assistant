e834f2d0d2d62a4c2b9529d4901b37cc1f83e12a
Neukiru <malkhazsuladze93@gmail.com>
2023-06-26 10:21:16 +0400

    add Chat panel

--- components/MutateGeneralStyle.jsx
+++ components/MutateGeneralStyle.jsx
@@ -5,9 +5,14 @@ const MutateGeneralStyle = () => {
   const router = useRouter()
   useEffect(() => {
     const mainElement = document.querySelector('main')
+    const paddingElement = document.querySelector('.nx-w-64')
     console.log(router.asPath)
     if (router.asPath === '/prompt_engineering/label/assistantReply') {
       mainElement.classList.remove('md:nx-px-12', 'nx-px-6')
+    }
+    else if(router.asPath === '/AIchat'){
+      mainElement.classList.remove('md:nx-px-12', 'nx-px-6')
+      paddingElement.classList.remove('nx-w-64')
     }
     else {
       mainElement.classList.add('md:nx-px-12', 'nx-px-6');
