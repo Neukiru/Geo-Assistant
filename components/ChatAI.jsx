0e158e5285b9906660635460f141617107799e58
Neukiru <malkhazsuladze93@gmail.com>
2023-06-28 12:07:09 +0400

    change endpoint

--- components/ChatAI.jsx
+++ components/ChatAI.jsx
@@ -4,8 +4,8 @@ import ChatBubble from './ChatBubble'
 import MessageBar from './MessageBar'
 import socketIOClient from 'socket.io-client'
 
-// const ENDPOINT = 'https://geo-assistant-backend.onrender.com/'
-const ENDPOINT = 'http://localhost:8000'
+const ENDPOINT = 'https://geo-assistant-backend.onrender.com/'
+// const ENDPOINT = 'http://localhost:8000'
 
 const Chat = ({ initialMessages }) => {
   const [messages, setMessages] = useState(initialMessages)
